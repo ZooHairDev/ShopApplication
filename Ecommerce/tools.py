47d606a849751f948545c8941e725837287b4240
Zouhair OB <46304063+ZooHairDev@users.noreply.github.com>
2024-02-16 12:32:50 +0100

    Add files via upload

---
+++ Ecommerce/tools.py
@@ -0,0 +1,13 @@
+# Define the tools of the application
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+
+def hash_pass(password):
+    password_hash = generate_password_hash(password)
+    return password_hash
+
+def verify_pass(provided_password, stored_password):
+    pwhash = stored_password
+    password = provided_password
+    return check_password_hash(pwhash, password)
